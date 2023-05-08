c90bb99ad1a44276ec2ab702936db5f95444c9b1
Antonio_nevesneto <126512175+Emile-a239@users.noreply.github.com>
2023-05-08 16:52:42 -0300

    Add files via upload

---
+++ programação web de toin/script.js
@@ -0,0 +1,36 @@
+//sla
+const name='aguinaldo'
+console.log=(name)
+console.log[typeofname]
+
+//declarar um número
+cost numero=4
+console.log(numero)
+console.log(typeof numero)
+//v ou f
+const boolean = false 
+console.log (boolean)
+console.log (typeof boolean)
+// lista 
+const linguagem = ["javascript";"php";"python"]
+console.log (linguagem)
+zonsole.log(typeof linguagem)
+// metodo string
+//contagem de caracteres de uma string
+cost fullname = 'aguinaldo filho'
+console.log(fullname. length)
+//string to array(lista)
+cost stringtoarray=fullname('')
+console.log(stringtoarray)
+// mudar o tipo da fonte
+console.log(fullname. tolowecase())
+console.log(touppercase())
+//encontrar uma palavra em uma string
+console.log(fullname.indexof)('filho')
+//inserindo elementos da lista
+list[s=f
+console.log(list)
+//resgatar uma parte de uma string
+console.log(fullname.slice(0,7))
+//metodos de arrays
+const.list=["a","]
