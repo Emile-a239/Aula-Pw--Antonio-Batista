144c4da1e020369f765b48aba363423713503958
Antonio_nevesneto <126512175+Emile-a239@users.noreply.github.com>
2023-05-11 14:17:29 -0300

    Add files via upload

---
+++ programação web de toin/script.js
@@ -0,0 +1,46 @@
+
+const name= 'aguinaldo'
+console.log (name)
+console.log (typeof name)
+
+//declarar um número.
+const number = 4
+console.log (number)
+console.log (typeof number)
+//v ou f.
+const boolean = false 
+console.log (boolean)
+console.log (typeof boolean)
+// lista.
+const linguage = ["javascript","php","python"]
+console.log (linguage)
+console.log (typeof linguage)
+// metodo string.
+//contagem de caracteres de uma string.
+const fullname = 'aguinaldo filho'
+console.log (fullname.length)
+//string to array(lista).
+const stringtoarray= 'fullname'
+console.log (stringtoarray)
+// mudar o tipo da fonte.
+console.log (fullname.toLowerCase)
+console.log (fullname.toUpperCase)
+//encontrar uma palavra em uma string.
+console.log(fullname.indexof)
+//resgatar uma parte de uma string.
+console.log(fullname.slice(0,7))
+//metodos de arrays.
+const list=['a','b','c','d','e']
+console. log (list.tength) //lista todod od elementos.
+console. log (list [2]) //acessar o elemento da lista.
+list.push ('g')
+list.pop()
+list.shift()
+list.unshift('a')
+const produtos={
+    nome:'camisa',
+    preco: 15.99,
+    estoque: true,
+    tamanho:['P','M','G']
+}
+
